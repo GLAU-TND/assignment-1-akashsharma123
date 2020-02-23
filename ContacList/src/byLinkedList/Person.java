214580ab807a8691ec1f8aa6eea73938a537fc29
Akash sharma <akash.sharma_cs18@gla.ac.in>
2020-02-24 00:15:42 +0530

    generate getter of the given field (FirstName,lastName,Contactnumber,emailID)

--- ContacList/src/byLinkedList/Person.java
+++ ContacList/src/byLinkedList/Person.java
@@ -1,4 +1,29 @@
 package byLinkedList;
 
+
+import java.util.ArrayList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private ArrayList<String> contactNumber;
+    private String mailId;
+
+    public String getMailId() {
+        return mailId;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public ArrayList<String> getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+
+
+    }
 }
